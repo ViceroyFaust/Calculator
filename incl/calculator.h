ac29d04ea7d33bfc56524c9357ae8131ddf99aef
Danylo Owl <d.ribchinski@gmail.com>
2021-08-15 14:06:04 -0400

    prototyped the header file for calculator class

---
+++ incl/calculator.h
@@ -0,0 +1,39 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_h
+#include <vector>
+
+class Calculator {
+    std::vector<double> stack;
+
+public:
+    // Stack functions
+    void push(double push);
+    void swap();
+    void roll();
+    void clear();
+    double pop();
+    int stack_size();
+    // Operation functions
+    void add();
+    void subtract();
+    void multiply();
+    void divide();
+    void mod();
+    // Functions
+    void negate();
+    void sin();
+    void cos();
+    void tan();
+    void square();
+    void inverse();
+    void pow();
+    void sqrt();
+    void log();
+    void ln();
+    void e_pow();
+    void sci_not();
+    // Constants
+    double pi();
+};
+
+#endif
