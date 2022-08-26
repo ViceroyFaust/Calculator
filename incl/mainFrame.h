8a637625ada924992f01e4f965e156e8068cb0ff
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2022-08-26 15:56:16 +0300

    Create wxButtons and ButtonIds

--- incl/mainFrame.h
+++ incl/mainFrame.h
@@ -3,9 +3,35 @@
 
 #include <wx/wx.h>
 
+enum ButtonId {
+    // Number Buttons
+    ZeroButtonId = wxID_LAST + 1,
+    OneButtonId,
+    TwoButtonId,
+    ThreeButtonId,
+    FourButtonId,
+    FiveButtonId,
+    SixButtonId,
+    SevenButtonId,
+    EightButtonId,
+    NineButtonId,
+    // Operation Buttons
+    AddButtonId,
+    SubtractButtonId,
+    MultiplyButtonId,
+    DivideButtonId,
+    // Misc Buttons
+    decimalButtonId,
+    signButtonId
+};
+
 class MainFrame : public wxFrame {
 private:
 public:
+
+wxButton* zero, one, two, three, four, five, six, seven, eight, nine,
+    add, subtract, multiply, divide, decimal, sign;
+
 MainFrame();
 ~MainFrame();
 
--- src/mainFrame.cpp
+++ src/mainFrame.cpp
@@ -1,7 +1,8 @@
 #include "mainFrame.h"
 
 MainFrame::MainFrame() : wxFrame(nullptr, wxID_ANY, "Calculator") {
-
+    wxButton* zero, one, two, three, four, five, six, seven, eight, nine,
+    eight;
 }
 
 MainFrame::~MainFrame() {}
